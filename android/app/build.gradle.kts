f6a456382243f9144a853937bd3c31bd18f58335
Hamidreza Alimirzaei <alimirzaei.hr@gmail.com>
2025-09-11 10:05:59 +0330

    بروز رسانی

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_application_1"
+        applicationId = "com.hamidrezaalimirzaei.ketabe_majara"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
